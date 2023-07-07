86bad5e901a0e6fd1637a0430bf8b3f1431ffea7
Ed <121364681+B-Edd@users.noreply.github.com>
2023-07-06 20:08:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,44 +9,71 @@ function searchBin() {
   var searchBox = document.getElementById("searchBox");
   var item = searchBox.value.toLowerCase();
   var result = document.getElementById("result");
-  var bin_with_s = item + 's'
-  var bin_with_es = item + 'es'
+  var bin_with_s = item + 's';
+  var bin_with_es = item + 'es';
 
   if (item in bins) {
     result.innerText = "Item goes in: " + bins[item] + '.';
-  } else if (bin_with_s in bins){
+  } else if (bin_with_s in bins) {
     result.innerText = "Item goes in: " + bins[bin_with_s] + '.';
   } else if (bin_with_es in bins) {
     result.innerText = "Item goes in: " + bins[bin_with_es] + '.';
   } else {
-      result.innerText = "Item not found.";
+    result.innerText = "Item not found.";
   }
 }
+
+function displayAutoCompleteList() {
+  var itemList = document.getElementById("itemList");
+  var searchBox = document.getElementById("searchBox");
+  var searchText = searchBox.value.toLowerCase();
+
+  // Clear existing options
+  itemList.innerHTML = "";
+
+  // Create an array of items from bins
+  var itemArray = Object.keys(bins);
+
+  // Sort the item array alphabetically
+  itemArray.sort();
+
+  // Add options for each item in sorted itemArray
+  for (var i = 0; i < itemArray.length; i++) {
+    var item = itemArray[i];
+    var option = document.createElement("option");
+
+    // Highlight matching letter in option text
+    var highlightedText = item.replace(new RegExp(searchText, "i"), "<strong>$&</strong>");
+
+    option.innerHTML = highlightedText;
+    itemList.appendChild(option);
+  }
+}
+
+
+// Display the autocomplete list on page load
+window.addEventListener("load", displayAutoCompleteList);
 
 function addItem() {
   var addItemBox = document.getElementById("addItemBox");
   var newItem = addItemBox.value.toLowerCase();
 
   if (newItem.trim() !== "") {
-    if (newItem in bins) {
-      alert("Item already exists in the bins.");
-    } else {
-      let which_bin = prompt("Which bin does it go in (recycling, garbage, compost)?: ").toLowerCase();
-
-      var variable = prompt("yo");
-      while (variable !== 'hi') {
-        let thing = prompt('Enter an item');
-        variable = prompt("Enter a bin");
-        bins[thing] = variable;
+    let which_bin = prompt("Which bin does it go in (recycling, garbage, compost)?: ").toLowerCase();
+    if (which_bin === 'recycling' || which_bin === 'garbage' || which_bin === 'compost') {
+        bins[newItem] = which_bin;
+        addItemBox.value = "";
+        saveToLocalStorage();
+        alert("New item added successfully!");
         displayDictionary();
-      }
+    } else {
+        alert('Please enter a valid item.');
     }
+
+
   } else {
     alert("Please enter a valid item.");
   }
-
-  addItemBox.value = "";
-  saveToLocalStorage();
 }
 
 
